07e14119f45649e1eb96c19cb7ba04133dc1e76e
Bastian <bacabezas2025@udec.cl>
2026-09-22 13:04:12 -0300

    fix: corregido error en código de redirections

---
+++ src/redirections.c
@@ -0,0 +1,45 @@
+#include "redirections.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*Funcion que redirecciona la salida y/o entrada de archivos.
+  archivoEntrada: El nombre del archivo para
+                  redireccionar la entrada (<), dejar NULL si no hay
+  archivoSalida: El nombre del archivo para redireccionar
+                 la salida (>), dejar NULL si no hay
+  append: Un verificador si el archivo de salida usa O_APPEND u O_TRUNC*/
+void redireccionar(char* archivoEntrada, char* archivoSalida, int append) {
+    if (archivoEntrada != NULL) {
+
+        //File descriptor para el archivo de entrada
+        int fdIn = open(archivoEntrada, O_RDONLY);
+        if (fdIn == -1) {
+            perror("Error al abrir archivo de entrada");
+            _exit(1);
+        }
+
+        dup2(fdIn, STDIN_FILENO);
+        close(fdIn);
+    }
+
+    if (archivoSalida != NULL) {
+        int flags = O_WRONLY | O_CREAT;
+        
+        //File descriptor del archivo de salida, se inicializa segun append
+        int fdOut;
+        if (append) {
+            int fdOut = open(archivoSalida, flags | O_APPEND, 0644);
+        } else {
+            int fdOut = open(archivoSalida, flags | O_TRUNC, 0644);
+        }
+
+        if (fdOut == -1) {
+            perror("Error al abrir archivo de salida");
+            _exit(1);
+        }
+        
+        dup2(fdOut, STDIN_FILENO);
+        close(fdOut);
+    }
+}
